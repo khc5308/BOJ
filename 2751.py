f0f0ee0b9dce12ad92c71cd655fa84e7e88e1d03
khc5308 <haechan0807303@naver.com>
2026-04-19 22:19:59 +0900

    파일 정리

--- 2751.py
+++
@@ -1,8 +0,0 @@
-n = int(input())
-num = [] 
-for _ in range (n):
-    num.append(int(input()))
-num.sort()
-
-for i in range (n):
-    print (num[i])
--- test.py
+++
@@ -1,6 +0,0 @@
-n = []
-for _ in range(28):
-    n.append(int(input()))
-for i in range(1,31):
-    if n.count(i) == 0:
-        print(i) 
