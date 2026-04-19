d86e1ff26f41f5c4460b3304bf04a445b5342a29
김해찬 <78535114+khc5308@users.noreply.github.com>
2026-04-19 22:50:19 +0900

    [Bronze IV] Title: 안밖? 밖안? 계단? 역계단?, Time: 88 ms, Memory: 108384 KB -BaekjoonHub

---
+++ 백준/Bronze/28290. 안밖？ 밖안？ 계단？ 역계단？/안밖？ 밖안？ 계단？ 역계단？.py
@@ -0,0 +1,4 @@
+try:
+    print(["in-out","in-out","out-in","out-in","stairs","reverse"][["fdsajkl;","jkl;fdsa","asdf;lkj",";lkjasdf","asdfjkl;",";lkjfdsa"].index(input())])
+except:
+    print("molu")
